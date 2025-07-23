67bcc7313cd79294df55542d40068e1c0636e7e0
Hari Krishnan <haripotter459@gmail.com>
2025-07-23 15:22:19 +0530

    have added the database url

--- database.py
+++ database.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 SQLALCHEMY_DATABASE_URL='sqlite:///./todosapp.db'
 
 
+SQLALCHEMY_DATABASE_URL_pdc = "oracle+oracledb://CM_EDWH_D:2022hzu+Zs@rb0orarac35.de.bosch.com:38000/?service_name=AST410_rb0orarac35.de.bosch.com"
 
 engine_auth=create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread':False})
 engine_pdc=create_engine(SQLALCHEMY_DATABASE_URL_pdc)
